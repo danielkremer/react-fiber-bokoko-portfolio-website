a20db9c6184b8d05b18aa2021ebe2a3e25996750
danielkremer <32129280+danielkremer@users.noreply.github.com>
2023-02-19 18:16:46 +0100

    created website

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: 'room',
+    type: 'glbModel',
+    path: '/models/my-room-v11.glb',
+  },
+  {
+    name: 'screen',
+    type: 'videoTexture',
+    path: '/textures/kda.mp4',
+  },
+];
